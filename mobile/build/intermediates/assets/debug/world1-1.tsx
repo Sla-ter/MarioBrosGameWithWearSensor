7c09b1399fba625ae80ffc6987a0ad9b3794cbbc
Sla-ter <40808581+Sla-ter@users.noreply.github.com>
2018-07-09 19:52:29 -0400

    Mario

---
+++ mobile/build/intermediates/assets/debug/world1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="4700" columns="188">
+ <image source="tiles.png" width="3392" height="460"/>
+</tileset>
